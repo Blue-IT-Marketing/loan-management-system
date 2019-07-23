99f9b611bc3f18cd401a2062f0eb4fd031f4510a
justice <mobiusndou@gmail.com>
2019-07-23 20:33:20 +0200

    init
    init

---
+++ src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="box box-footer with-border">
+            <footer className="main-footer">
+                <div className="pull-right hidden-xs">
+                    <b>Midey </b> <a href="#"> Finance</a>
+                </div>
+                <div className="pull-left hidden-xs">
+                <strong>Copyright &copy; 2019 <a href="#">Midey</a>.</strong></div> All rights reserved  
+                </footer>
+        </div>    
+        )
+}
---
+++ src/constants/routes.js
@@ -0,0 +1,35 @@
+
+
+export const home_page = '/';
+export const contact_page = '/contact';
+export const about_page = '/about';
+export const login_page = '/login';
+export const logout_page = '/logout';
+export const signup_page = '/signup';
+export const forget_password_page ='/forget';
+export const download_page = '/download';
+export const profile_page = '/profile';
+
+export const blog_page = '/blog';
+export const admin_page = '/admin';
+export const dashboard_page = '/dashboard';
+
+
+
+export const register_employee = '/admin/employees/register';
+
+
+export const leads = '/leads';
+
+export const loans = '/loans';
+
+export const loan_payments = '/loans/payments';
+
+export const employee_profile = '/employees/profiles/';  //+ employee_id
+
+
+export const chat = '/chat';
+
+export const employee_admin = '/employees/admin';
+
+
